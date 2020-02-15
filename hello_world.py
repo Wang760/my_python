70ee7a8e3a7eb8f2703302e98fd870caff55fe38
Wang760 <richard.wang@acoustictek.com>
2020-02-15 21:09:05 +0800

    学习python第二天

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,21 @@
+'''
 print('Hello, world!')
 print(12)
 print('please input your name: ')
 name = input()
 print('hello '+ name)
+'''
+
+hp = 3814
+max_hp=3480
+result= int(hp/max_hp)
+result *=100
+print(str(result) + '%')
+
+my_string = 'qweasdzxc'
+print(my_string[::-1])
+print(my_string[:3])
+print(my_string[3:])
+
+
+
